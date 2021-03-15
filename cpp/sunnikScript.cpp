8657a9fb496f86543671cd4ab066663da7ade7f3
sunnigtv <61459888+sunnigtv@users.noreply.github.com>
2021-03-15 20:33:46 +0900

    Add files via upload

--- cpp/sunnikScript.cpp
+++ cpp/sunnikScript.cpp
@@ -1,10 +1,8 @@
-#include "include/sunnikScript.h"
+﻿#include "sunnikScript.h"
+using namespace Ss;
 
 int main(void)
 {
-	Ss::asmd::a(); //+계산기 함수입니다.
-	Ss::asmd::s(); //-계산기 함수입니다.
-	Ss::asmd::m(); //×계산기 함수입니다.
-	Ss::asmd::d(); //÷계산기 함수입니다.
-	Ss::
+	int dd = 0;
+	login::login();
 }
